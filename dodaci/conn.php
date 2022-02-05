8c2cef388d2f33e6cd0536d24cffa68143d43c87
Ivan Šušnjara <48264772+isusnjara0@users.noreply.github.com>
2022-02-05 12:24:20 +0100

    Update conn.php

--- dodaci/conn.php
+++ dodaci/conn.php
@@ -3,7 +3,7 @@
  $dbhost = "eu-cdbr-west-02.cleardb.net";
  $dbuser = "b33e7d0dcf19eb";
  $dbpass = "8c344f99";
- $db = "`heroku_ee10db9e69e6457`";
+ $db = "heroku_ee10db9e69e6457";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) ;
  
  if (mysqli_connect_errno())
